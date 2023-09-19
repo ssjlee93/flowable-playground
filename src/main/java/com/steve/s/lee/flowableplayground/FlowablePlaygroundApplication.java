587466e3b36ad842c7a9f616d535ab5fad800664
Steve S. Lee <sjlee12@email.com>
2023-09-19 15:00:12 -0400

    init

---
+++ src/main/java/com/steve/s/lee/flowableplayground/FlowablePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.steve.s.lee.flowableplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowablePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowablePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steve/s/lee/flowableplayground/FlowablePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.s.lee.flowableplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowablePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
